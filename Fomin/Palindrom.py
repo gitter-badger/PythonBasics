02a13f87125bad2e6c68f88607b163a3b4938943
Dedefer <ddf1998@gmail.com>
2015-10-12 21:35:37 +0300

    last 10 numbers, that returns last 10 numbers of sum 1^1 + 2^2 + ... + 1377^1377, and some improvements in palindrom.py

--- Fomin/Palindrom.py
+++ Fomin/Palindrom.py
@@ -1,10 +1,11 @@
-n = 0
+n = [0,[]]
 for i in range(1,13702):
 	b = str(i)
 	c = 0
 	while b != b[-1::-1]:
 		if c == 51:
-			n += 1
+			n[0] += 1
+			n[1].append(i)
 			break
 		b = str(int(b) + int(b[-1::-1]))
 		c += 1
---
+++ Fomin/last 10 numbers.py
@@ -0,0 +1,2 @@
+a = sum(i ** i for i in range(1,1378))
+print(str(a)[-10:])
