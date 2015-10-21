2fe4333d4e677eaa4a33e049a9832e8cd649d0b2
Dedefer <ddf1998@gmail.com>
2015-10-22 00:41:36 +0300

    Мозг поплыл, третий коммит за полчаса

--- Fomin/Prime numbers (Eratosthenos).py
+++ Fomin/Prime numbers (Eratosthenos).py
@@ -1,11 +1,10 @@
 n = int(input('Input N '))
 a = [i for i in range(1,n+1)]
 i = 1
-m = len(a)
 while i != len(a):
-	n = a[i]
+	m = a[i]
 	a = set(a)
-	b = {n*c for c in range(2, m // n + 1)}
+	b = {m*c for c in range(2, n // m + 1)}
 	a = list(a - b)
 	i += 1
 a.sort()
